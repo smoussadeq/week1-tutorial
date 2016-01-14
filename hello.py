f11312906588e86803017e26325146b04de9d765
Said Moussadeq <saidmoussadeq@saids-MacBook-Pro.local>
2016-01-13 23:17:24 -0500

    step 10 and 11 instructions

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is said'
+	return 'hello world, my name is '
 
-print( hello_world() + 'hector' )
+print( hello_world() + ‘said’ )
